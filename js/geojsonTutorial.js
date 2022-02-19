3aad03b981df03730b8df146f4b39bc7ee96dc89
Gengkui Liu <91988224+wellagainst@users.noreply.github.com>
2022-02-19 10:56:34 -0600

    template map complete

--- js/geojsonTutorial.js
+++ js/geojsonTutorial.js
@@ -1,6 +1,6 @@
-var map = L.map('map', {
-    center: [47.609179, -122.337693],
-    zoom: 13
-});
+//var map = L.map('map', {
+    //center: [47.609179, -122.337693],
+    //zoom: 13
+//});
 
-L.geoJSON(map).addTo(mymap);
+//L.geoJSON(map).addTo(mymap);
