8c77ca9c719ec0230de89dbc2ae6f3866d35fa24
Gengkui Liu <91988224+wellagainst@users.noreply.github.com>
2022-03-06 09:50:38 -0600

    implementing Lab 1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /* Map of GeoJSON data from MegaCities.geojson */
 //declare map var in global scope
 var map;
+var dataStats = {};
 var minValue;
 //function to instantiate the Leaflet map
 function createMap(){
@@ -53,7 +54,7 @@ function createMap(){
 };
 
 
-function calculateMinValue(data){
+function calcStats(data){
     
     //create empty array to store all data values
     var allValues = [];
@@ -72,11 +73,12 @@ function calculateMinValue(data){
     
         }
     }
-    console.log(allValues)
-    //get minimum value of our array
-    var minValue = Math.min(...allValues)
-    
-    return minValue;
+    //get min, max, mean stats for our array
+    dataStats.min = Math.min(...allValues);
+    dataStats.max = Math.max(...allValues);
+    //calculate meanValue
+    var sum = allValues.reduce(function(a, b){return a+b;});
+    dataStats.mean = sum/ allValues.length;
 }
 
 
@@ -118,12 +120,11 @@ function pointToLayer(feature, latlng, attributes){
     var layer = L.circleMarker(latlng, options);
 
     //build popup content string
-    var popupContent = "<p><b>State:</b> " + feature.properties.State + "</p>";
-    var year = attribute.split("_")[1];
-    popupContent += "<p><b>HIV newly diagnoses rate in " + year + ":</b> " + feature.properties[attribute] + " %</p>";
+    var popupContent = new PopupContent(feature.properties, attribute);
+    
     //bind the popup to the circle marker
-    layer.bindPopup(popupContent,{
-        offset: new L.Point(0,-options.radius) 
+    layer.bindPopup(popupContent.formatted, { 
+        offset: new L.Point(0,-options.radius)
     });
     
     //return the circle marker to the L.geoJson pointToLayer option
@@ -145,17 +146,37 @@ function createPropSymbols(data, attributes){
 
 //Step 1: Create new sequence controls
 function createSequenceControls(attributes){
-    //create range input element (slider)
-    var slider = "<input class='range-slider' type='range'></input>";
-    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+    var SequenceControl = L.Control.extend({
+        options: {
+            position: 'bottomleft'
+        },
+
+        onAdd: function () {
+            
+            // create the control container div with a particular class name
+            var container = L.DomUtil.create('div', 'sequence-control-container');
+
+            //create range input element (slider)
+            container.insertAdjacentHTML('beforeend', '<input class="range-slider" type="range">')
+
+            //add skip buttons
+            container.insertAdjacentHTML('beforeend','<button class="step" id="reverse">Last Year</button>'); 
+            container.insertAdjacentHTML('beforeend','<button class="step" id="forward">Next Year</button>');
+            //disable any mouse event listeners for the container
+            L.DomEvent.disableClickPropagation(container);
+
+            return container;
+        }
+    });
+
+    map.addControl(new SequenceControl());
+
     //set slider attributes
     document.querySelector(".range-slider").max = 4;
     document.querySelector(".range-slider").min = 0;
     document.querySelector(".range-slider").value = 0;
     document.querySelector(".range-slider").step = 1;
-    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse">Reverse</button>');
-    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward">Forward</button>');
-    
+
     //Step 5: click listener for buttons
     document.querySelectorAll('.step').forEach(function(step){
         step.addEventListener("click", function(){
@@ -179,8 +200,6 @@ function createSequenceControls(attributes){
         
     })
     
-
-
     //Step 5: input listener for slider
     document.querySelector('.range-slider').addEventListener('input', function(){            
         //Step 6: get the new index value
@@ -188,8 +207,43 @@ function createSequenceControls(attributes){
         //console.log(index)
         updatePropSymbols(attributes[index]);
     });
-    
-    
+};
+
+
+function createLegend(attributes){
+    var LegendControl = L.Control.extend({
+        options: {
+            position: 'bottomright'
+        },
+
+        onAdd: function () {
+            // create the control container with a particular class name
+            var container = L.DomUtil.create('div', 'legend-control-container');
+            container.innerHTML = '<p class="temporalLegend">HIV newly diagnoses rate in <span class="year">2015</span></p>';
+            //Step 1: start attribute legend svg string
+            var svg = '<svg id="attribute-legend" width="160px" height="60px">';
+            //array of circle names to base loop on
+            var circles = ["max", "mean", "min"];
+
+            //Step 2: loop to add each circle and text to svg string
+            for (var i=0; i<circles.length; i++){
+                //Step 3: assign the r and cy attributes  
+                var radius = calcPropRadius(dataStats[circles[i]]);  
+                var cy = 59 - radius;
+                //circle string
+                svg += '<circle class="legend-circle" id="' + circles[i] + '" r="' + radius + '"cy="' + cy + '" fill="#F47821" fill-opacity="0.8" stroke="#000000" cx="30"/>';
+            };
+
+            //close svg string
+            svg += "</svg>";
+
+            //add attribute legend svg to container
+            container.insertAdjacentHTML('beforeend',svg);
+            return container;
+        }
+    });
+
+    map.addControl(new LegendControl());
     
 };
 
@@ -205,17 +259,22 @@ function updatePropSymbols(attribute){
             layer.setRadius(radius);
 
             //add city to popup content string
-            var popupContent = "<p><b>State:</b> " + props.State + "</p>";
-
-            //add formatted attribute to panel content string
-            var year = attribute.split("_")[1];
-            popupContent += "<p><b>HIV newly diagnoses rate in " + year + ":</b> " + props[attribute] + " %</p>";
+            var popupContent = new PopupContent(props, attribute);
 
             //update popup content            
             popup = layer.getPopup();            
-            popup.setContent(popupContent).update();
+            popup.setContent(popupContent.formatted).update();
         };
     });
+};
+
+
+function PopupContent(properties, attribute){
+    this.properties = properties;
+    this.attribute = attribute;
+    this.year = attribute.split("_")[1];
+    this.formatted = "<p><b>HIV newly diagnoses rate in " + this.year + ":</b> " + this.properties[attribute] + " %</p>";
+
 };
 
 
@@ -249,8 +308,7 @@ function getData(){
         })
         .then(function(json){
             var attributes = processData(json);
-            //calculate minimum data value
-            minValue = calculateMinValue(json);
+            calcStats(response);
             //call function to create proportional symbols
             createPropSymbols(json, attributes);
             createSequenceControls(attributes);
